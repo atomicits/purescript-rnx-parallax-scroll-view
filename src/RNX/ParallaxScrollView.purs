c025ec172bc1772f716e15eb1336650cbb1e4494
vishnuprasad <vishnuprasad.s@atomicits.com>
2017-01-27 14:51:42 +0530

    delete unused action type

--- src/RNX/ParallaxScrollView.purs
+++ src/RNX/ParallaxScrollView.purs
@@ -8,44 +8,44 @@ import React (createElement, ReactElement, ReactClass)
 
 foreign import parallaxScrollViewClass :: forall props. ReactClass props
 
-parallaxScrollView :: forall action. Array Props -> Array ReactElement -> ReactElement
+parallaxScrollView :: Array Props -> Array ReactElement -> ReactElement
 parallaxScrollView props = createElement parallaxScrollViewClass (unsafeFromPropsArray props)
 
-pxBackgroundColor :: forall action. Color -> Props
-pxBackgroundColor = unsafeMkProps "backgroundColor"
+pxBackgroundColor :: Color -> Props
+pxBackgroundColor c = unsafeMkProps "backgroundColor" (show c)
 
-pxBackgroundSpeed :: forall action. Int -> Props
+pxBackgroundSpeed :: Int -> Props
 pxBackgroundSpeed = unsafeMkProps "backgroundSpeed"
 
-pxContentBackgroundColor :: forall action. Color -> Props
-pxContentBackgroundColor = unsafeMkProps "contentBackgroundColor"
+pxContentBackgroundColor :: Color -> Props
+pxContentBackgroundColor c = unsafeMkProps "contentBackgroundColor" (show c)
 
-pxFadeOutForeground :: forall action. Boolean -> Props
+pxFadeOutForeground :: Boolean -> Props
 pxFadeOutForeground = unsafeMkProps "fadeOutForeground"
 
-pxOnChangeHeaderVisibility :: forall action. (Boolean -> Unit) -> Props
+pxOnChangeHeaderVisibility :: (Boolean -> Unit) -> Props
 pxOnChangeHeaderVisibility = unsafeMkProps "onChangeHeaderVisibility"
 
-pxParallaxHeaderHeight :: forall action. Int -> Props
+pxParallaxHeaderHeight :: Int -> Props
 pxParallaxHeaderHeight = unsafeMkProps "parallaxHeaderHeight"
 
-pxRenderBackground :: forall action. ( Unit -> ReactElement)  -> Props
+pxRenderBackground :: (Unit -> ReactElement)  -> Props
 pxRenderBackground = unsafeMkProps "renderBackground"
 
-pxRenderFixedHeader :: forall action. ( Unit -> ReactElement ) -> Props
+pxRenderFixedHeader :: (Unit -> ReactElement) -> Props
 pxRenderFixedHeader = unsafeMkProps "renderFixedHeader"
 
-pxRenderForeground :: forall action. ( Unit -> ReactElement ) -> Props
+pxRenderForeground :: (Unit -> ReactElement) -> Props
 pxRenderForeground = unsafeMkProps "renderForeground"
 
-pxRenderScrollComponent :: forall action. (Props -> ReactElement)  -> Props
+pxRenderScrollComponent :: (Props -> ReactElement)  -> Props
 pxRenderScrollComponent = unsafeMkProps "renderScrollComponent"
 
-pxRenderStickyHeader :: forall action. ( Unit -> ReactElement ) -> Props
+pxRenderStickyHeader :: (Unit -> ReactElement) -> Props
 pxRenderStickyHeader = unsafeMkProps "renderStickyHeader"
 
-pxStickyHeaderHeight :: forall action. Int -> Props
+pxStickyHeaderHeight :: Int -> Props
 pxStickyHeaderHeight = unsafeMkProps "stickyHeaderHeight"
 
-pxContentContainerStyle :: forall action. StyleId -> Props
+pxContentContainerStyle :: StyleId -> Props
 pxContentContainerStyle = unsafeMkProps "contentContainerStyle"
